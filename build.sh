1c6c896b2b780b8420f384fffa4dba3125cf97ad
Edward M <edwardmanasseh@gmail.com>
2025-11-08 14:23:45 +0100

    render update

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+ 
+# Backend dependencies
+pip install -r backend/requirements.txt
+ 
+# Frontend build
+echo "Building frontend..."
+cd frontend
+npm install
+# Give execution permission to react-scripts
+chmod +x node_modules/.bin/react-scripts
+npm run build
+cd ..
+ 
+# Copy frontend build to backend static folder
+echo "Copying frontend build to backend..."
+cp -a frontend/build/. backend/static/
