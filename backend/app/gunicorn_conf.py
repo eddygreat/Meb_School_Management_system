810f439cc229c85a1088faa586e1220dbb3b0ffa
Edward M <edwardmanasseh@gmail.com>
2025-11-26 11:36:01 +0100

    tracking backend

--- backend/app/gunicorn_conf.py
+++ backend/app/gunicorn_conf.py
@@ -6,7 +6,7 @@ import multiprocessing
 # Server socket
 # Bind to 0.0.0.0 to allow external connections (e.g., from Docker's host)
 # The port is set from an environment variable, defaulting to 8080.
-bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
+bind = f"0.0.0.0:{os.getenv('PORT', '8080')} --timeout 120"
 
 # Worker processes
 # A common recommendation is (2 * number_of_cores) + 1.
